ebb6855c00f018e9bf2c2f38ee12ca4aadf478d0
source-architect <source.architect@gmail.com>
2021-08-30 17:50:45 +0430

    fix portfolio section bug

--- app.js
+++ app.js
@@ -1,64 +1,55 @@
-// ------ Navbar Background Chagne On Scroll ------
-$(function(){
-	var navbar = $('#my-navbar');
-	
-	$(window).scroll(function(){
-		if($(window).scrollTop() <= 40){
-			navbar.removeClass('onScroll');
-		} else {
-			navbar.addClass('onScroll');
-		}
-	});
-});
-
-// ------ Navbar Collapse ------
-var collapseBtn = document.querySelector('.navbar-toggler');
-var navSection = document.querySelector('nav');
-collapseBtn.addEventListener('click', function(){
-  navSection.classList.toggle('my-collapse');
-})
-
-// ------ Navbar Offset ------
-var offset = 72;
-$('.navbar li a').click(function(event) {
-    event.preventDefault();
-    $($(this).attr('href'))[0].scrollIntoView();
-    scrollBy(0, -offset);
-});
-
-
-// ------ Filter Gallery ------
-var filterBtns = document.querySelectorAll('.filter-nav > button');
-for(const cur of filterBtns){
-  cur.addEventListener('click', function(){
-    for(var elem of filterBtns){
-      elem.classList.remove('active');
-    }
-    cur.classList.add('active');
-  });
-}
-
 $(function () {
+  // ------ Isotope setup ------
+  "use strict";
 
-  'use strict';
-
-  var $projects = $('.projects');
+  var $projects = $(".projects");
 
   $projects.isotope({
-      itemSelector: '.item',
-      layoutMode: 'fitRows'
+    itemSelector: ".item",
+    layoutMode: "fitRows",
   });
 
-  $('.filter-nav > button').on('click', function(){
-
-      var filter = $(this).attr('data-filter');
-      $projects.isotope({
-          filter: filter
-      });
+  $(".filter-nav > button").on("click", function () {
+    var filter = $(this).attr("data-filter");
+    $projects.isotope({
+      filter: filter,
+    });
   });
 
+  // ------ Filter Gallery ------
+  var filterBtns = document.querySelectorAll(".filter-nav > button");
+  for (const cur of filterBtns) {
+    cur.addEventListener("click", function () {
+      for (var elem of filterBtns) {
+        elem.classList.remove("active");
+      }
+      cur.classList.add("active");
+    });
+  }
+
+  // ------ Navbar Background Chagne On Scroll ------
+  var navbar = $("#my-navbar");
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() <= 40) {
+      navbar.removeClass("onScroll");
+    } else {
+      navbar.addClass("onScroll");
+    }
+  });
+
+  // ------ Navbar Collapse ------
+  var collapseBtn = document.querySelector(".navbar-toggler");
+  var navSection = document.querySelector("nav");
+  collapseBtn.addEventListener("click", function () {
+    navSection.classList.toggle("my-collapse");
+  });
+
+  // ------ Navbar Offset ------
+  var offset = 72;
+  $(".navbar li a").click(function (event) {
+    event.preventDefault();
+    $($(this).attr("href"))[0].scrollIntoView();
+    scrollBy(0, -offset);
+  });
 });
-
-
-
-
